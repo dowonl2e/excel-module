c9508409cddcdfa5f3615a073da2646670a00639
dowonl2e <dierwdod@gmail.com>
2024-01-16 12:41:33 +0900

    커스텀 Cell Draw 삭제

--- src/main/java/com/study/excel/draw/CustomExcelCellDraw.java
+++ src/main/java/com/study/excel/draw/CustomExcelCellDraw.java
@@ -1,39 +1,36 @@
 package com.study.excel.draw;
 
-import org.apache.poi.ss.usermodel.Drawing;
-import org.apache.poi.ss.usermodel.Font;
-
-import com.study.excel.configurer.ExcelCellCommentConfigurer;
-import com.study.excel.configurer.ExcelCellPictureConfigurer;
-
-public class CustomExcelCellDraw implements ExcelCellDraw {
-
-	private ExcelCellCommentConfigurer commentConfigurer = new ExcelCellCommentConfigurer();
-	private ExcelCellPictureConfigurer pictureConfigurer = new ExcelCellPictureConfigurer();
-	
-	public CustomExcelCellDraw comment(int startColIndex, int startRowIndex, int endColIndex, int endRowIndex, String commentStr) {
-		commentConfigurer.comment(startColIndex, startRowIndex, endColIndex, endRowIndex, commentStr);
-		return this;
-	}
-	
-	public CustomExcelCellDraw picture(int startColIndex, int startRowIndex, int endColIndex, int endRowIndex, String commentStr) {
-		commentConfigurer.comment(startColIndex, startRowIndex, endColIndex, endRowIndex, commentStr);
-		return this;
-	}
-	
-	@Override
-	public void applyComment(Drawing drawing) {
-		commentConfigurer.configure(drawing);
-	}
-	
-	@Override
-	public void applyComment(Drawing drawing, Font font) {
-		commentConfigurer.configure(drawing, font);
-	}
-
-	@Override
-	public void applyPicture(Drawing drawing) {
-		
-	}
+public class CustomExcelCellDraw {
 
 }
+//public class CustomExcelCellDraw implements ExcelCellDraw {
+//
+//	private ExcelCellCommentConfigurer commentConfigurer = new ExcelCellCommentConfigurer();
+//	private ExcelCellPictureConfigurer pictureConfigurer = new ExcelCellPictureConfigurer();
+//
+//	public CustomExcelCellDraw comment(int startColIndex, int startRowIndex, int endColIndex, int endRowIndex, String commentStr) {
+//		commentConfigurer.comment(startColIndex, startRowIndex, endColIndex, endRowIndex, commentStr);
+//		return this;
+//	}
+//
+//	public CustomExcelCellDraw picture(int startColIndex, int startRowIndex, int endColIndex, int endRowIndex, String commentStr) {
+//		commentConfigurer.comment(startColIndex, startRowIndex, endColIndex, endRowIndex, commentStr);
+//		return this;
+//	}
+//
+//	@Override
+//	public void applyComment(Drawing drawing) {
+//		commentConfigurer.configure(drawing);
+//	}
+//
+//	@Override
+//	public void applyComment(Drawing drawing, Font font) {
+//		commentConfigurer.configure(drawing, font);
+//	}
+//
+//	@Override
+//	public void applyPicture(Drawing drawing) {
+//
+//	}
+//
+//}
